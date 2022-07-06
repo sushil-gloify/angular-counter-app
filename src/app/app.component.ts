83a091d709a5d06ac8f9490c42a6a2c18900bb1e
SushilDhore <sushil@gloify.com>
2022-07-06 14:59:40 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'counterapp';
+  
+
+  countNumber = 0;
+  resetBtn ()
+  {
+    this.countNumber = 0;
+
+  }
+
+  increaseBtn() 
+  {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  decreaseBtn()
+  {
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber<0)
+    {
+      this.countNumber =  0;
+    }
+  }
+
+}
